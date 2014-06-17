42c2181df46fc726b06086da42d71a7e395c3bd7
Greg Chu <Chu_greg@yahoo.com>
2014-06-17 00:03:42 -0400

    added on touch

--- app/src/main/java/com/gregchu/f4/app/Fragment1.java
+++ app/src/main/java/com/gregchu/f4/app/Fragment1.java
@@ -9,17 +9,20 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
 /**
  * Created by Vivz on 10/25/13.
  */
-public class Fragment1 extends Fragment {
+public class Fragment1 extends Fragment implements View.OnTouchListener {
+
+    private static final String Fragment1 = "Fragment1";
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-        Log.d("VIVZ","onAttach");
+        Log.d(Fragment1,"onAttach");
 
     }
 
@@ -28,11 +31,11 @@ public class Fragment1 extends Fragment {
         super.onCreate(savedInstanceState);
         if(savedInstanceState==null)
         {
-            Log.d("VIVZ","onCreate FIRST TIME");
+            Log.d(Fragment1,"onCreate FIRST TIME");
         }
         else
         {
-            Log.d("VIVZ","onCreate SUBSEQUENT TIME");
+            Log.d(Fragment1,"onCreate SUBSEQUENT TIME");
         }
 
     }
@@ -40,70 +43,105 @@ public class Fragment1 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater,container,savedInstanceState);
-        Log.d("VIVZ","onCreateView");
-        return inflater.inflate(R.layout.fragment_1, container, false);
+        Log.d(Fragment1,"onCreateView");
+        View view = inflater.inflate(R.layout.fragment_1, container, false);
+        view.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+
+                Log.d(Fragment1,"onTouch");
+                if(event.getAction() == MotionEvent.ACTION_MOVE){
+                    Log.d(Fragment1,"onTouch move");
+                }
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    Log.d(Fragment1,"onTouch down");
+                }
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    Log.d(Fragment1,"onTouch up");
+                }
+                return true;
+            }
+        });
+
+        return view;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        Log.d("VIVZ","onActivityCreated");
+        Log.d(Fragment1,"onActivityCreated");
 
     }
+
+
 
     @Override
     public void onStart() {
         super.onStart();
-        Log.d("VIVZ","onStart");
+        Log.d(Fragment1,"onStart");
 
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        Log.d("VIVZ","onResume");
+        Log.d(Fragment1,"onResume");
 
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        Log.d("VIVZ","onPause");
+        Log.d(Fragment1,"onPause");
 
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        Log.d("VIVZ","onSaveInstanceState");
+        Log.d(Fragment1,"onSaveInstanceState");
 
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        Log.d("VIVZ","onStop");
+        Log.d(Fragment1,"onStop");
 
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        Log.d("VIVZ","onDestroyView");
+        Log.d(Fragment1,"onDestroyView");
 
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.d("VIVZ","onDestroy");
+        Log.d(Fragment1,"onDestroy");
 
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        Log.d("VIVZ","onDetach");
+        Log.d(Fragment1,"onDetach");
 
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        Log.d(Fragment1,"onTouch");
+        if(event.getAction() == MotionEvent.ACTION_MOVE){
+            Log.d(Fragment1,"onTouch move");
+        }
+        if(event.getAction() == MotionEvent.ACTION_DOWN){
+            Log.d(Fragment1,"onTouch down");
+        }
+        if(event.getAction() == MotionEvent.ACTION_UP){
+            Log.d(Fragment1,"onTouch up");
+        }
+        return true;
     }
 }
